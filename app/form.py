ea15880339dbe9440c61bcb9f6f4c88a09aada8c
Alexander Sotishvili <a.sotishvili@smartweb.com.ua>
2020-06-19 14:14:53 +0300

    added custom validation amount

--- app/form.py
+++ app/form.py
@@ -1,7 +1,8 @@
+from decimal import Decimal
+
 from flask_wtf import FlaskForm
 from wtforms import DecimalField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import InputRequired, Length
-
+from wtforms.validators import InputRequired, Length, ValidationError
 
 from app.utils import EUR_CURRENCY, USD_CURRENCY, RUB_CURRENCY
 
@@ -18,3 +19,9 @@ class PaymentForm(FlaskForm):
         "Описание", validators=[InputRequired(), Length(max=250)]
     )
     submit = SubmitField("Оплатить")
+
+    def validate_amount(self, field):
+        if field.data and field.data.quantize(Decimal("1.00")) < 0:
+            raise ValidationError('Amount must be positive')
+        if field.data.quantize(Decimal("1.00")) == 0:
+            raise ValidationError('Amount can not be ziro')
